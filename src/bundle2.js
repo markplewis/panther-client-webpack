a989851c251d45f4e44a349e4baabc9887542526
markplewis <markplewis1@gmail.com>
2018-09-23 10:10:02 -0400

    Updates and fixes

---
+++ src/bundle2.js
@@ -0,0 +1,2 @@
+// Important: must import main SCSS file into the JS entry point file
+import "./scss/bundle2.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
@@ -8,18 +8,19 @@ module.exports = (env, argv) => {
   const fileLoaderConfig = {
     loader: "file-loader",
     options: {
-      // Transform the resolved asset file path
+      // Rewrite the resolved asset file path and tell Webpack where to save the files
       // regExp: /\/node_modules\/tgam-patterns\/assets\/patterns\/([^.]+)/,
-      name: (file) => {
+      name: file => {
         // return argv.mode === "development" ? "resources/[1].[ext]" : "resources/[1]-[hash].[ext]";
         const pieces = file.replace(__dirname, "").replace("/node_modules/tgam-patterns/assets/patterns", "/tgam-patterns").split(".");
         pieces.pop();
-        let trimmed = pieces.join(".");
-        if (trimmed.startsWith("/")) {
-          // Remove leading slash
-          trimmed = trimmed.substr(1, trimmed.length);
-        }
-        return argv.mode === "development" ? `resources/${trimmed}.[ext]` : `resources/${trimmed}-[hash].[ext]`;
+        const trimmed = pieces.join(".");
+        // let trimmed = pieces.join(".");
+        // if (trimmed.startsWith("/")) {
+        //   trimmed = trimmed.substr(1, trimmed.length); // Remove leading slash
+        // }
+        // return argv.mode === "development" ? `resources/${trimmed}.[ext]` : `resources/${trimmed}-[hash].[ext]`;
+        return argv.mode === "development" ? `resources${trimmed}.[ext]` : `resources${trimmed}-[hash].[ext]`;
       }
       // name: "[path][name].[ext]"
       // context: path.resolve(__dirname, "node_modules/tgam-patterns")
@@ -27,20 +28,25 @@ module.exports = (env, argv) => {
   };
 
   return {
+    // See: https://webpack.js.org/configuration/stats/
+    // stats: "verbose",
     entry: {
-      main: "./src/index.js"
+      main: "./src/main.js",
+      bundle2: "./src/bundle2.js"
     },
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: argv.mode === "development" ? "[name].js" : "[name]-[chunkhash].js"
     },
+    // See: https://webpack.js.org/configuration/resolve
     resolve: {
       alias: {
+        // Make it easier to @import tgam-patterns styles into this project's SASS files
         "panther": "tgam-patterns/assets/patterns",
         // Once "string-replace-loader" has run, the asset paths will start
-        // with "./tgam" instead of "~assets", so we'll need to make them
-        // resolve properly. See: https://webpack.js.org/configuration/resolve
-        "./tgam": path.resolve(__dirname, "node_modules/tgam-patterns/assets")
+        // with "./tgam" instead of "~assets/patterns", so we'll need to make
+        // them resolve properly
+        "./tgam": path.resolve(__dirname, "node_modules/tgam-patterns/assets/patterns")
       }
     },
     module: {
@@ -63,9 +69,11 @@ module.exports = (env, argv) => {
             },
             "postcss-loader",
             {
+              // Replace the first part of the asset file path so that
+              // it can be resolved (see the "resolve" config above)
               loader: "string-replace-loader",
               options: {
-                search: "~assets", 
+                search: "~assets/patterns", 
                 replace: "tgam",
                 flags: "g"
               }
@@ -89,13 +97,21 @@ module.exports = (env, argv) => {
     plugins: [
       new CleanWebpackPlugin("dist", {}),
       new MiniCssExtractPlugin({
-        filename: argv.mode === "development" ? "style.css" : "style-[contenthash].css"
+        filename: argv.mode === "development" ? "[name].css" : "[name]-[contenthash].css"
       }),
+      // Could use this to make things more dynamic:
+      // https://github.com/mutualofomaha/multipage-webpack-plugin
       new HtmlWebpackPlugin({
         inject: false,
         hash: false,
         template: "./src/index.html",
         filename: "index.html"
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        hash: false,
+        template: "./src/bundle2.html",
+        filename: "bundle2.html"
       })
     ]
   }
