6388c3731a2e875187c4f77f65a5f680d731ed60
markplewis <markplewis1@gmail.com>
2018-09-27 08:08:42 -0400

    More work

--- src/js/bundle3.js
+++ src/js/bundle3.js
@@ -1,5 +1,8 @@
 import "../scss/bundle3.scss";
 
 import "tgam/images/nav-arrow-down.svg";
+import "tgam/images/icon-snapchat.svg";
+import "tgam/images/icon-facebook.svg";
+import "tgam/images/icon-twitter.svg";
 
 console.log("Hello, I'm bundle 3");
